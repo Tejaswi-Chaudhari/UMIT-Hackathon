6e4f8095c71c11f6a8b576c0a9f5ec1bf9493032
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-11-27 22:37:03 +0530

    judges

--- src/components/Judges/Judges.jsx
+++ src/components/Judges/Judges.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
+import React from 'react';
+import "./Judges.css";
 
 const Judges = () => {
     return (
-        <div>
-            <h1>This is Judges</h1>
-        </div>
+        <>
+
+            <div>
+              
+                <div class="container py-3">
+              
+                    <div class="row text-center text-black">
+                        <div class="col-lg-8 mx-auto">
+                            <h1 class="display-4">JUDGES</h1>
+      
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="container">
+                    <div class="row text-center">
+
+
+                        <div class="col-xl-3 col-sm-6 mb-5">
+                            <div class="bg-grey rounded shadow-sm py-5 px-4 "><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                <h5 class="mb-0">xyz</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                                <ul class="social mb-0 list-inline mt-3">
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <div class="col-xl-3 col-sm-6 mb-5">
+                            <div class="bg-grey rounded shadow-sm py-5 px-4 "><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                <h5 class="mb-0">xyz</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                                <ul class="social mb-0 list-inline mt-3">
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <div class="col-xl-3 col-sm-6 mb-5">
+                            <div class="bg-grey rounded shadow-sm py-5 px-4 "><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                <h5 class="mb-0">xyz</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                                <ul class="social mb-0 list-inline mt-3">
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <div class="col-xl-3 col-sm-6 mb-5">
+                            <div class="bg-grey rounded shadow-sm py-5 px-4 "><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                <h5 class="mb-0">xyz</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                                <ul class="social mb-0 list-inline mt-3">
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 

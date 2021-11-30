b8f2aaf4b4d8d648ef9b29eca7a99c888a61ee97
sneha1026 <93974699+sneha1026@users.noreply.github.com>
2021-11-30 20:49:47 +0530

    Update Sponsors.jsx

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -2,9 +2,94 @@ import React from 'react'
 
 const Sponsors = () => {
     return (
-        <div>
-            <h1>This is sponsors</h1>
+    <div class="container-fluid">
+      <h2 align="center" >SPONSORS</h2>  
+
+<div class="row">
+  <div class="col-md-3">
+    <div class="thumbnail">
+        <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" class="img-circle" alt="Sponsor 1" style="width:50%">
+        <div class="caption">
+          <p align="center">Sponsor 1</p>
         </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="thumbnail">
+        <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" class="img-circle" alt="Sponsor 2" style="width:50%">
+        <div class="caption">
+          <p align="center">Sponsor 2</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="thumbnail">
+        <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" class="img-circle" alt="Sponsor 3" style="width:50%">
+        <div class="caption">
+          <p align="center">Sponsor 3</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="thumbnail">
+        <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" class="img-circle" alt="Sponsor 4" style="width:50%">
+        <div class="caption">
+          <p align="center">Sponsor 4</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="thumbnail">
+        <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" class="img-circle" alt="Sponsor 5" style="width:50%">
+        <div class="caption">
+          <p align="center">Sponsor 5</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="thumbnail">
+        <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" class="img-circle" alt="Sponsor 6" style="width:50%">
+        <div class="caption">
+          <p align="center">Sponsor 6</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="thumbnail">
+        <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" class="img-circle" alt="Sponsor 7" style="width:50%">
+        <div class="caption">
+         <p align="center">Sponsor 7</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="thumbnail">
+        <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" class="img-circle" alt="Sponsor 8" style="width:50%">
+        <div class="caption">
+          <p align="center">Sponsor 8</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+
+ </div>
+</div>
+
     )
 }
 

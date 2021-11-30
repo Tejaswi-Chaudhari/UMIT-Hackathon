0d79c647f6f0eb0c542720ed329f154c98ae66ff
shrutigavali <gavalishruti21@gmail.com>
2021-12-01 00:38:01 +0530

    Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import {React} from 'react'
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import "./Header.css" ;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUsers, faUser, faList, faAward, faTrophy, faPoll, faQuestion, faCalendarAlt, faAddressBook } from "@fortawesome/free-solid-svg-icons"
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <Navbar fixed="top" collapseOnSelect expand="lg"  variant="light" className="main">
+            <Container>
+        
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                </Nav> 
+                <Nav>
+                <Nav.Link className="link" href="#home">Home</Nav.Link>
+                <Nav.Link className="link" href="#about">About</Nav.Link>
+                <Nav.Link className="link" href="#events">Timeline</Nav.Link>
+                <Nav.Link className="link" href="#team">Categories</Nav.Link>
+                <Nav.Link className="link" href="#tests">Prizes</Nav.Link>
+                <Nav.Link className="link" href="#blog">Sponsors</Nav.Link>
+                <Nav.Link className="link" href="#contact">Judges</Nav.Link>
+                <Nav.Link className="link" href="#contact">Team</Nav.Link>
+                <Nav.Link className="link" href="#contact">FAQ</Nav.Link>
+                <Nav.Link className="link" href="#contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 

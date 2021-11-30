003afd456e7736e9079ba11d4347d8518621742e
swarangimp05 <swarangimp05@gmail.com>
2021-11-30 11:32:49 +0530

    teams updated

--- src/components/Teams/Teams.jsx
+++ src/components/Teams/Teams.jsx
@@ -1,9 +1,108 @@
 import React from 'react'
+import { Carousel } from "react-bootstrap"
+
+import teams from "./teams.jpg"
+
 
 const Teams = () => {
     return (
         <div>
-            <h1>This is Teams</h1>
+            {/* <h1>Teams</h1> */}
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 teamImg"
+                        src={teams}
+                        alt="First slide"
+                        height="650px"
+                    />
+                    <Carousel.Caption>
+                        <h3>GDSC</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={teams}
+                        alt="Second slide"
+                        height="650px"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>IEEE</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={teams}
+                        alt="Third slide"
+                        height="650px"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>CSI</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={teams}
+                        alt="First slide"
+                        height="650px"
+                    />
+                    <Carousel.Caption>
+                        <h3>CodeChef</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={teams}
+                        alt="First slide"
+                        height="650px"
+                    />
+                    <Carousel.Caption>
+                        <h3>ACM</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={teams}
+                        alt="First slide"
+                        height="650px"
+                    />
+                    <Carousel.Caption>
+                        <h3>E-cell</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={teams}
+                        alt="First slide"
+                        height="650px"
+                    />
+                    <Carousel.Caption>
+                        <h3>WIE</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+            </Carousel>
+
+
         </div>
     )
 }

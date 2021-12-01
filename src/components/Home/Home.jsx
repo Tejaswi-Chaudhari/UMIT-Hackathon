954a284a329fb7cda4d655ebb4d3cfdbfe31c5a5
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-12-02 02:20:17 +0530

    home UI in progress

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ import "./Home.css";
 
 const Home = () => {
 
-    const image1 = "https://images.unsplash.com/photo-1536650135175-9b3cd4f36cff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
+    const image1 = "https://images.unsplash.com/photo-1547082299-de196ea013d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
 
     return (
         <div>
@@ -17,17 +17,23 @@ const Home = () => {
                         <div className='home-title'>
                             Arcturus
                         </div>
-                        <div className='home-countdown'>
-                            <center>
-                                <iframe src="https://free.timeanddate.com/countdown/i837fkqv/cf100/cm0/cu4/ct0/cs1/ca0/co0/cr0/ss1/cacfff/cpcfff/pct/tcfff/fs400/szw448/szh189/iso2022-01-25T00:00:00" allowtransparency="true" frameborder="0" width="448" height="189"></iframe>
-                            </center>
-                        </div>
-                        <div className='home-nav'>
-                            <center>
-                                <Button variant='danger' className='home-nav-discord'>Discord</Button>
-                                <Button variant='danger' className='home-nav-devfolio'>Devfolio</Button>
-                            </center>
-                        </div>
+                        <center>
+                            <div className='home-box'>
+                                <div className='home-countdown'>
+                                    <center>
+                                        <iframe src="https://free.timeanddate.com/countdown/i837fkqv/cf100/cm0/cu4/ct0/cs1/ca0/co0/cr0/ss1/cacfff/cpcfff/pct/tcfff/fs400/szw448/szh189/iso2022-01-25T00:00:00" allowtransparency="true" frameborder="0" width="448" height="189"></iframe>
+                                    </center>
+                                </div>
+                                <div className='home-nav'>
+                                    <center>
+                                        <Button variant='danger' className='home-nav-discord'>Discord</Button>
+                                        <Button variant='danger' className='home-nav-devfolio'>Devfolio</Button>
+                                    </center>
+                                    <br></br>
+                                    <br></br>
+                                </div>
+                            </div>
+                        </center>
                     </div>
                 </Parallax>
             </div>

64b3c20b3b24a31af6df323899bcffaf1cb3dc3b
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-12-01 23:40:38 +0530

    sync

--- src/components/FAQs/FAQs.jsx
+++ src/components/FAQs/FAQs.jsx
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
-import {questions} from './api';
-import './FAQs.css';
-import MyAccordion from './MyAccordion';
+// import React, {useState} from 'react';
+// import {questions} from './api';
+// import './FAQs.css';
+// import MyAccordion from './MyAccordion';
 
 const FAQ=() =>{
-    const [faqs, setfaqs] = useState(questions);
+    // const [faqs, setfaqs] = useState(questions);
 
    return (
         <>
-            {
+            {/* {
                 faqs.map((curElem)=> {
                     const {id}=curElem;
                     return <MyAccordion key={id} {...curElem}/>
                 })
-             }
+             } */}
         </>
     )
 }
-export default FAQs;
+export default FAQ;

1bca3af66aaa7ed7d797dc191be0806d00ba45e7
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2022-01-11 17:02:09 +0530

    FAQs updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,13 +13,10 @@ const About = () => {
                         <div data-animate="fadeInUp" class="col-lg-6 text">
                             <p>We-Accinge <i>(W-Accinge)</i> a <strong>Hackathon</strong> of Usha Mittal Institute of Technology. A collaborative effort of GDSC, IEEE, CodeChef, CSI, WIE, ACM and E-Cell UMIT. We Accinge means bracing ourselves for giving our best. We motivate young and curious minds to be a part of this hackathon, give their best and learn.</p>
                             <p>'A finger cannot lift a weight but, the strength of multiple hands can'. Like us, we encourage you to team-up and give it a shot.</p>
-                            <p className='lead about-l-txt'>Team Size: 1-4 Participants<br></br>Eligibility Criteria: Students Only</p>
-                            <Button className='about-btn btn-grad'>
-                                Check out the domains
-                            </Button>
-                            <Button className='about-btn btn-grad'>
-                                Register Now
-                            </Button>
+                            <p className='lead about-l-txt'>Team Size: 1-4 Participants<br></br>Eligibility Criteria: Students Only (Undergraduate)</p>
+                            <a href="https://discord.gg/ZgHe8egwDq" target="_blank" className='about-btn'>
+                                Join our <strong>Discord</strong> server to get further updates
+                            </a>
                         </div>
                         <div class="col-lg-6">
                             <Image src={about} className='about-img'></Image>
--- src/components/FAQs/FAQs.jsx
+++ src/components/FAQs/FAQs.jsx
@@ -7,13 +7,49 @@ function App() {
         {
             id: 1,
             question: 'How many people should a Team be?',
-            answer: 'A team should be between 1-4 people',
+            answer: 'A team should be between 1-4 people.',
         },
         {
             id: 2,
             question: 'What are the costs of participating?',
             answer: 'Free of cost',
         },
+        {
+            id: 2,
+            question: 'What are the costs of participating?',
+            answer: 'Free of cost',
+        },
+        {
+            id: 3,
+            question: 'What are the tracks of the Hackathon?',
+            answer: 'FinTech, Education, Healthcare, Gaming and innovation, Agriculture, Social Cause',
+        },
+        {
+            id: 4,
+            question: 'Can we have team members who are not from UMIT?',
+            answer: 'Yes, you can have team members from any college.',
+        },
+        {
+            id: 5,
+            question: 'Is a specific coding language required?',
+            answer: 'No, We do not have any restriction as far as coding language is concerned.',
+        },
+        {
+            id: 6,
+            question: 'Can I submit an old project?',
+            answer: 'No, We do not encourage this. 70%+ of the work submitted under this hackathon must be between the coding period instructed.',
+        },
+        {
+            id: 7,
+            question: 'Are there any pre-events? If so is participation necessary in these events?',
+            answer: 'Yes, There are pre-events (Tech Talks). The participation is voluntary.',
+        },
+        {
+            id: 8,
+            question: 'Will each team get the mentor?',
+            answer: 'Yes, Each team will get a mentor who will assist them throughtout the coding period.',
+        },
+        
     ])
 
     const [clicked, setClicked] = useState("0");
